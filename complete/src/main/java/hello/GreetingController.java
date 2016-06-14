9420441ba7604c838f05dd2c044138d2ab4a9a69
Michael Moghadas <mmoghadas@salesforce.com>
2016-06-14 13:50:01 -0700

    checking to see if JSONObject fails to create json if string contains tab character

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -2,8 +2,12 @@ package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.json.JSONObject;
+import io.vertx.core.json.JsonObject;
 
 @RestController
 public class GreetingController {
@@ -15,5 +19,13 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    public String test(@RequestBody String data) {
+        JSONObject json = new JSONObject(data);
+//        JsonObject json = new JsonObject(data);
+        System.out.println(json);
+        return data;
     }
 }
